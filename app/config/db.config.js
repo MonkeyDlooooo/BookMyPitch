64b8a68e99bc0948ff107c27fb6f14fde33608c6
Huynh Chi Tin <fpthuynhchitin@gmail.com>
2024-12-11 16:40:54 +0700

    update csdl

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,14 +1,8 @@
-require('dotenv').config();
 const Sequelize = require('sequelize');
-// Khởi tạo kết nối MySQL thông qua thông tin từ process.env
-const sequelize = new Sequelize(
-  'bookmypitch', 
-  'root', 
-  '', {
-    dialect: 'mysql',
-    host: "localhost",
-    logging: false, // tắt log
-  }
-);
+const sequelize = new Sequelize('bookmypitch', 'root', '', {
+  dialect: 'mysql',
+  host: 'localhost',
+  logging: false, // tắt log
+});
 
 module.exports = sequelize;
